dd05bd32bef4d0dbd91d3537d4c186e93c097da2
cjade <862226994@qq.com>
2017-04-06 21:41:44 +0800

    创建后台用户控制器

---
+++ app/Http/Controllers/Admin/UsersController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chengyu
+ * Date: 17/4/6
+ * Time: 下午9:01
+ */
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+
+class UsersController extends Controller
+{
+    /**
+     * @action index 管理员列表
+     */
+    public function index()
+    {
+
+    }
+}
