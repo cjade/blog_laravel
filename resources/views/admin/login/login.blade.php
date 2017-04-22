637c4b1b4b46fb91a1fe10a2332dd04704e9ee42
cjade <862226994@qq.com>
2017-04-23 01:03:54 +0800

    登录js文件

--- resources/views/admin/login/login.blade.php
+++ resources/views/admin/login/login.blade.php
@@ -111,16 +111,18 @@
 <!-- /.login-box -->
 
 <!-- jQuery 2.2.3 -->
-{{--<script src="assets/plugins/jQuery/jquery-2.2.3.min.js"></script>--}}
+<script src="assets/plugins/jQuery/jquery-2.2.3.min.js"></script>
 <!-- Bootstrap 3.3.6 -->
-<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+{{--<script src="assets/bootstrap/js/bootstrap.min.js"></script>--}}
 <!-- iCheck -->
 {{--<script src="assets/plugins/iCheck/icheck.min.js"></script>--}}
+
 
 <script src="https://unpkg.com/vue/dist/vue.js"></script>
 <!-- 引入组件库 -->
 <script src="https://unpkg.com/element-ui/lib/index.js"></script>
 <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+<script src="js/admin/login.js"></script>
 <style>
     .form-group{
         margin-top: 10px;
@@ -133,68 +135,6 @@
         text-align: center;
     }
 </style>
-<script>
-    new Vue({
-        el: '#app',
-        data: function(){
-            return {
-                loading: false,
-                ruleForm: {
-                    email: '',
-                    password: ''
-                },
-                rules:{
-                    email: [
-                        {required: true,  message: '请填写邮箱地址', trigger: 'blur'},
-                        {type: "email", message: '邮箱格式错误', trigger: 'blur'}
-                    ],
-                    password: [
-                        {required: true, message: '请填写密码', trigger: 'blur'},
-                        {min: 6, max: 16, message: '密码长度在 6 到 16 个字符', trigger: 'blur'}
-                    ]
-                }
-            }
-        },
-        methods: {
-            submitForm(ruleForm) {
-                var _this = this;
-                var _duration = 2 * 1000;
-                _this.$refs[ruleForm].validate((valid)=>{
-                    if (valid) {
-                        _this.loading = true;
-                        axios.post('login', _this.ruleForm).then(function (response) {
-                            console.log(response);
-                            let data = response.data;
-                            console.log(data);
-                            if (data.status == 1) {
-                                _this.$message({
-                                    message: data.info,
-                                    type:'success',
-                                    duration: _duration
-                                });
-                                setTimeout(function () {
-                                    window.location.href='index';
-                                }, _duration);
-                            } else {
-                                _this.$message.error(data.info);
-                                _this.loading = false;
-                            }
-                        }).catch(function (error) {
-                            _this.loading = false;
-                            console.log(error);
-                        });
-                    } else {
-                        console.log('error submit!!');
-                        return false;
-                    }
-                });
-            },
-            resetForm(formName) {
-                this.$refs[formName].resetFields();
-            }
-        }
-    })
 
-</script>
 </body>
 </html>
